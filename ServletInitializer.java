1cc2a940bd382d537038d5f72da2085a1432f3d3
Akshaya HK <akhk4639@gmail.com>
2025-09-22 11:51:00 +0530

    name chnaged

---
+++ ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.multipurpose;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HYNO_Management.class);
+	}
+
+}
